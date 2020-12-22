9b32ca54bcfe2eb8d88864e8dfa59b1a69e595c5
Haris Khan <theHarisKhan@users.noreply.github.com>
2020-12-22 12:15:31 -0800

    Work on API requsts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
+  const APP_KEY = 'nPAwglGw20GtTIU6EIQ2M707gVGYacFg'
+
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    GetRequest()
+  },[])
+
+  const GetRequest = async () => {
+    const response = await fetch(
+     ` https://api.nytimes.com/svc/books/v3/reviews.json?title=Deacon+King+Kong&api-key=${APP_KEY}`
+
+    )
+    const data = await response.json()
+    setBooks(data.results)
+    console.log(data.results)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Books review App</h1>
+      {books.map(book => (
+        <>
+          <h1>{book.book_title}</h1>
+          <img src={`http://covers.openlibrary.org/b/isbn/${book.isbn13[0]}-M.jpg`} alt={book.book_title} />
+          <h3>{book.book_author}</h3>
+        </>
+        // console.log(book.isbn13[0])
+      ))}
     </div>
   );
 }
